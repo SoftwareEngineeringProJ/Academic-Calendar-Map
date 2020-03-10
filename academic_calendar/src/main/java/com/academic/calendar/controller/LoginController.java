c751e94034ddb2458e85cb51ef78177bba635e00
JonnyS123 <1933794630@qq.com>
2020-03-10 17:28:46 +0800

    0.1-完成前端网页草稿

--- academic_calendar/src/main/java/com/academic/calendar/controller/LoginController.java
+++ academic_calendar/src/main/java/com/academic/calendar/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.academic.calendar.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -15,10 +16,10 @@ public class LoginController {
      * 测试代码
      * @return
      */
-    @RequestMapping("/test")
-    @ResponseBody
+    @RequestMapping(value = "/action",method = RequestMethod.GET)
     public String testSpringBoot(){
-        return "springboot成功";
+        System.out.println("test");
+        return "index";
     }
 
 }
