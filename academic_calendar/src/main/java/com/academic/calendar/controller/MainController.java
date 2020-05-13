f3c437456124c24452de907e9f0aed59846e300f
JonnyS1226 <1933794630@qq.com>
2020-05-13 15:11:35 +0800

    删除无用文件

---
+++ academic_calendar/src/main/java/com/academic/calendar/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.academic.calendar.controller;
+
+/**
+ * 总控制器
+ */
+public class MainController {
+}
---
+++ academic_calendar/src/main/java/com/academic/calendar/dao/AcadCalMapDao.java
@@ -0,0 +1,12 @@
+package com.academic.calendar.dao;
+
+/**
+ * 学术日历地图总体dao层接口（草稿）
+ *      使用mybatis，所以不用写实现类
+ */
+public interface AcadCalMapDao {
+
+
+
+
+}
